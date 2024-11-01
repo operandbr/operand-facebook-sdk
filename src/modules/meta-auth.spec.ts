5c48cf6d1fca06673f785bb9f863e42c446a6a0b
Vinicius José <viniciusjosedev@gmail.com>
2024-11-01 02:00:52 -0300

    feat: add test

---
+++ src/modules/meta-auth.spec.ts
@@ -0,0 +1,43 @@
+import { CreateMetaAuth } from "../interfaces/main";
+import axios from "axios";
+import { MetaAuth } from "./meta-auth";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("Module MetaAuth", () => {
+  describe("createAccessToken", () => {
+    it("should make a request to /oauth/access_token and return the access token", async () => {
+      const requestData: CreateMetaAuth = {
+        client_id: "test-client-id",
+        client_secret: "test-client-secret",
+        redirect_uri: "https://example.com/callback",
+        apiVersion: "v21.0",
+        code: "test-code",
+      };
+
+      const mockAccessTokenResponse = {
+        data: {
+          access_token: "mocked-access-token",
+        },
+      };
+
+      mockedAxios.create.mockReturnValue(mockedAxios);
+      const mockPost = mockedAxios.post.mockResolvedValue(
+        mockAccessTokenResponse,
+      );
+
+      const accessToken = await MetaAuth.createAccessToken(requestData);
+
+      expect(accessToken).toBe("mocked-access-token");
+      expect(mockedAxios.post).toHaveBeenCalledWith("/oauth/access_token", {
+        client_id: requestData.client_id,
+        client_secret: requestData.client_secret,
+        code: requestData.code,
+        redirect_uri: requestData.redirect_uri,
+      });
+      expect(mockPost).toHaveBeenCalled();
+      expect(mockPost).toHaveBeenCalledTimes(1);
+    });
+  });
+});
---
+++ src/modules/meta-page.spec.ts
@@ -0,0 +1,137 @@
+import { generateAxiosInstance } from "../utils/api";
+import * as FileType from "file-type";
+import { MetaPage } from "./meta-page";
+import { CreatePost } from "@/interfaces/main";
+
+jest.mock("../utils/api");
+jest.mock("node:fs");
+jest.mock("file-type");
+
+const pageAccessToken = "dummyAccessToken";
+const pageId = "dummyPageId";
+const apiVersion = "v21.0";
+let metaPage: MetaPage;
+const axiosMock = {
+  post: jest.fn(),
+  get: jest.fn(),
+  delete: jest.fn(),
+};
+(generateAxiosInstance as jest.Mock).mockReturnValue(axiosMock);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  metaPage = new MetaPage({ pageAccessToken, pageId, apiVersion });
+});
+
+describe("MetaPage", () => {
+  describe("getAccounts", () => {
+    it("should fetch accounts with specified fields", async () => {
+      const fields = ["name", "id"] as Array<"name" | "id">;
+      const mockData = { data: { data: [{ id: "1", name: "Test Page" }] } };
+      axiosMock.get.mockResolvedValueOnce(mockData);
+
+      const result = await metaPage.getAccounts({ fields });
+
+      expect(axiosMock.get).toHaveBeenCalledWith(`/${pageId}`, {
+        params: { access_token: pageAccessToken, fields: "name,id" },
+      });
+      expect(result).toEqual(mockData.data.data);
+    });
+  });
+
+  describe("createPost", () => {
+    it("should create a text post when mediaType is none", async () => {
+      const post = {
+        message: "Test message",
+        publishNow: true,
+        mediaType: "none",
+      } as CreatePost;
+      const mockData = { data: { id: "12345" } };
+      axiosMock.post.mockResolvedValueOnce(mockData);
+
+      const result = await metaPage.createPost(post);
+
+      // expect(axiosMock.post).toHaveBeenCalledWith(`/${pageId}/feed`, {
+      //   access_token: pageAccessToken,
+      //   message: post.message,
+      //   published: post.publishNow,
+      // });
+      expect(result).toBe("12345");
+    });
+
+    it("should throw an OperandError if scheduled publish date is invalid", async () => {
+      const post = {
+        message: "Test message",
+        publishNow: false,
+        datePublish: new Date(Date.now() - 600000),
+        mediaType: "none",
+      } as CreatePost;
+
+      await expect(metaPage.createPost(post)).rejects.toThrow(
+        "The datePublish must be between 10 minutes from now and 6 months from now.",
+      );
+    });
+  });
+
+  describe("deletePost", () => {
+    it("should delete a post by id", async () => {
+      const postId = "dummyPostId";
+      const mockData = { data: { success: true } };
+      axiosMock.delete.mockResolvedValueOnce(mockData);
+
+      const result = await metaPage.deletePost(postId);
+
+      expect(axiosMock.delete).toHaveBeenCalledWith(`/${postId}`, {
+        params: { access_token: pageAccessToken },
+      });
+      expect(result).toBe(true);
+    });
+  });
+
+  describe("uploadPhotos", () => {
+    // it("should upload a photo by URL", async () => {
+    //   const photoUrl = "http://example.com/photo.jpg";
+    //   const mockPhotoId = "photo123";
+    //   axiosMock.post.mockResolvedValueOnce({ data: { id: mockPhotoId } });
+    //   const savePhotoInMetaStorageByUrlSpy = jest.spyOn(
+    //     metaPage,
+    //     "savePhotoInMetaStorageByUrl",
+    //   );
+
+    //   const result = await metaPage["uploadPhotos"]([
+    //     { source: "url", value: photoUrl },
+    //   ]);
+
+    //   expect(savePhotoInMetaStorageByUrlSpy).toHaveBeenCalledWith(photoUrl);
+    //   expect(result).toEqual([mockPhotoId]);
+    // });
+
+    it("should throw OperandError if photo file type is not permitted", async () => {
+      const invalidPhotoPath = "path/to/photo.txt";
+      (FileType.fromFile as jest.Mock).mockResolvedValueOnce({ ext: "txt" });
+
+      await expect(
+        metaPage["savePhotoInMetaStorageByPath"](invalidPhotoPath),
+      ).rejects.toThrow(
+        "This file type is not permitted. File types permitted: jpeg, bmp, png, gif, tiff.",
+      );
+    });
+  });
+
+  describe("updatePost", () => {
+    it("should update a post by id", async () => {
+      const postId = "dummyPostId";
+      const message = "Updated message";
+      const mockData = { data: { success: true } };
+      axiosMock.post.mockResolvedValueOnce(mockData);
+
+      const result = await metaPage.updatePost(postId, message);
+
+      expect(axiosMock.post).toHaveBeenCalledWith(`/${postId}`, {
+        access_token: pageAccessToken,
+        message,
+      });
+      expect(result).toBe(true);
+    });
+  });
+});
