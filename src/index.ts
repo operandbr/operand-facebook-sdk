beb45478111148ec2d269ee4ee75b83ed7d25459
Vinicius José <viniciusjosedev@gmail.com>
2024-11-01 16:16:17 -0300

    feat: add export interfaces and types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { MetaAuth } from "./modules/meta-auth";
 import { MetaPage } from "./modules/meta-page";
+import * as IMain from "./interfaces/main";
+import * as IMeta from "./interfaces/meta";
 
-export { MetaAuth, MetaPage };
+export { MetaAuth, MetaPage, IMain, IMeta };
