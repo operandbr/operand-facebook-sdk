7398a03700a02ca8d99e946ccee22b7eeea370d5
Vinicius José <viniciusjosedev@gmail.com>
2024-12-03 14:52:19 -0300

    feat: add MetaIng module and update exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { MetaAuth } from "./modules/meta-auth";
 import { MetaPage } from "./modules/meta-page";
-import * as IMain from "./interfaces/meta-page";
-import * as IMeta from "./interfaces/meta-response";
+import { MetaIng } from "./modules/meta-ing";
+import * as IMetaAuth from "./interfaces/meta-auth";
+import * as IMetaPage from "./interfaces/meta-page";
+import * as IMetaIng from "./interfaces/meta-ing";
+import * as IMetaResponse from "./interfaces/meta-response";
 
-export { MetaAuth, MetaPage, IMain, IMeta };
+export {
+  MetaAuth,
+  MetaPage,
+  MetaIng,
+  IMetaAuth,
+  IMetaPage,
+  IMetaIng,
+  IMetaResponse,
+};
