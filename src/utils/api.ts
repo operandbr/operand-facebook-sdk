68e544f9473fa0b6a3ad92885e60261ae13be2e9
Vinicius José <viniciusjosedev@gmail.com>
2025-01-23 12:06:29 -0300

    feat: add timeout configuration to Axios instance creation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,13 +5,17 @@ export const generateAxiosInstance = (
   apiVersion: ApiVersion,
   isVideoApi?: boolean,
 ) => {
+  const timeout = 60000;
+
   if (isVideoApi) {
     return axios.create({
       baseURL: `https://graph-video.facebook.com/${apiVersion}`,
+      timeout,
     });
   }
 
   return axios.create({
     baseURL: `https://graph.facebook.com/${apiVersion}`,
+    timeout,
   });
 };
