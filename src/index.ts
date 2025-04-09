346dd9fc75dd1609d31ab982169d0d0fc9a134ae
Vinicius José <viniciusjosedev@gmail.com>
2025-04-09 11:22:38 -0300

    feat: add export OperandError

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { MetaAuth } from "./modules/auth/meta-auth";
 import { MetaPage } from "./modules/meta-page";
 import { MetaIng } from "./modules/meta-ing";
 import { MetaMkt } from "./modules/meta-mkt";
+import { OperandError } from "./error/operand-error";
 import * as IMetaAuth from "./interfaces/meta-auth";
 import * as IMetaPage from "./interfaces/page-publish";
 import * as IMetaIng from "./interfaces/ing-publish";
@@ -12,6 +13,7 @@ export {
   MetaPage,
   MetaIng,
   MetaMkt,
+  OperandError,
   IMetaAuth,
   IMetaPage,
   IMetaIng,
